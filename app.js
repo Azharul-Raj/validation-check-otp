83b67c1e485291f42cc12ac1d4907d8048dc883b
Azharul-Raj <azharulislam.raj512@gmail.com>
2022-08-19 23:02:06 +0600

    five digit number generated function done

--- app.js
+++ app.js
@@ -3,4 +3,12 @@ function generateNumber() {
     const number = Math.round(Math.random() * 100000)
     return number
 }
-
+function getPin() {
+    const generatedPin = generateNumber() + ''
+    if (generatedPin.length === 5) {
+        return generatedPin
+    }
+    else {
+       return getPin()
+    }
+}
