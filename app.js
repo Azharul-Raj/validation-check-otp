c8854bd6df3194fd8e6360dee09710a4a13604dd
Azharul-Raj <azharulislam.raj512@gmail.com>
2022-08-20 00:56:48 +0600

    veryfication done

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function getPin() {
 }
 
 // console.log(getPin())
-document.getElementById('get-pin').addEventListener('click', function () {
+document.getElementById('generate-pin').addEventListener('click', function () {
     const pinNumber = getPin()
     const display = document.getElementById('pin-display')
     display.value = pinNumber
@@ -25,6 +25,7 @@ document.getElementById('calculator').addEventListener('click', function (event)
     const number = event.target.innerText
     const displayField = document.getElementById('type-number')
     const previousNumber = displayField.value
+    
     if (number === 'C') {
         displayField.value = ''
     } 
@@ -34,9 +35,29 @@ document.getElementById('calculator').addEventListener('click', function (event)
         const escape = digits.join('')
         displayField.value = escape
     }
+    if (isNaN(number)) {
+        return
+    }
     else {
         const displayValue = previousNumber + number
         displayField.value = displayValue
 
+    }
+})
+
+document.getElementById('verify-btn').addEventListener('click', function () {
+    const display = document.getElementById('pin-display')
+    const verify = display.value
+    const displayField = document.getElementById('type-number')
+    const previousNumber = displayField.value
+    const successMsg = document.getElementById('success-msg')
+    const declinMsg = document.getElementById('declin-msg')
+    if (verify === previousNumber) {
+        successMsg.style.display = 'block'
+        declinMsg.style.display = 'none'
+    }
+    else {
+        declinMsg.style.display = 'block'
+        successMsg.style.display = 'none'
     }
 })
